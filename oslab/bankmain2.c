c8967e7ef1a2ecf6d398d1c72defc5cda5b8517d
DEMONLAWYERGT <91903192+DEMONLAWYERGT@users.noreply.github.com>
2022-10-18 21:04:59 +0530

    Update bankmain2.c

--- oslab/bankmain2.c
+++ oslab/bankmain2.c
@@ -17,47 +17,51 @@ return flag;
 }
 int main()
 {
- printf("\nEnter number of process and resources respectively\n");
+ printf("\nEnter number of process and resources respectively:- \n");
  scanf("%d%d",&pno,&res);
- printf("\n enter allocation matrix :- \n");
+ printf("\nEnter allocation matrix :- \n");
  for(i=0;i<pno;i++)
  {
- printf("Enter row %d\n",i+1);
+ printf("Enter row %d:- \n",i+1);
  for(j=0;j<res;j++)
  scanf("%d",&allocation[i][j]);
  }
- printf("\nenter max matrix :- \n");
+ printf("\nEnter max matrix :- \n");
  for(i=0;i<pno;i++)
  {
  printf("Enter row %d\n",i+1);
  for(j=0;j<res;j++)
  scanf("%d",&max[i][j]);
  }
- printf("\nallocation matrix is\n");
+ printf("\nAllocation matrix is :-\n");
  for(i=0;i<pno;i++)
  {
  for(j=0;j<res;j++)
  printf("%d\t",allocation[i][j]);
  printf("\n");
  }
- printf("\nmax matrix is :-\n");
+ printf("\nMax matrix is :- \n");
  for(i=0;i<pno;i++)
  {
  for(j=0;j<res;j++)
  printf("%d\t",max[i][j]);
  printf("\n");
  }
- printf("\nenter available resource :- \n");
+ printf("\nEnter available resource :- \n");
  for(i=0;i<res;i++)
  scanf("%d",&available[i]);
- //calculation of need matrix
  for(i=0;i<pno;i++)
  for(j=0;j<res;j++)
  need[i][j]= max[i][j]-allocation[i][j];
- //assign work array
  for(i=0;i<res;i++)
  work[i]=available[i];
-
+printf("Max Matrix:-\n");
+for(i=0;i<pno;i++)
+ {
+ for(j=0;j<res;j++)
+ printf("%d\t",need[i][j]);
+ printf("\n");
+ }
  int flag=0,flag1=0;
  do
  {
@@ -83,14 +87,10 @@ int main()
  need[i][j]=0;
  }
  printf("P%d -> ",i+1);
- }
- }
- }
- }
-
- while(check()==1);
-printf(" is the safe state");
-
-
- return 0;
+ }}}}
+while(check()==1);
+printf(" is the safe state.\n");
+while(check()==0);
+printf(" is the unsafe state.\n");
+return 0;
  }
