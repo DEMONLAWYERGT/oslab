c059d22f6352f0a6c94f05e9c903b08159cc21cc
DEMONLAWYERGT <91903192+DEMONLAWYERGT@users.noreply.github.com>
2022-10-25 11:20:17 +0530

    Update scandisksh.c

--- oslab/scandisksh.c
+++ oslab/scandisksh.c
@@ -3,7 +3,6 @@ void main()
 {
         int ioq[20],i,n,j,ihead,temp,scan,tot;
         float seek=0,avgs;
-        
         printf("Enter the number of requests\t:");
         scanf("%d",&n);
         printf("Enter the initial head position\t:");
@@ -15,8 +14,8 @@ void main()
         for(i=2;i<n;i++){
                 scanf("%d",&ioq[i]);        
         }
-        
-        for(i=0;i<n-1;i++){
+                for(i=0;i<n-1;i++)
+        {
                 for(j=0;j<n-1;j++)
                 {
                 
@@ -27,8 +26,7 @@ void main()
                                 ioq[j+1] = temp;       
                                 
                         }             
-                                
-                }
+                    }
         }
         ioq[n]=ioq[n-1];
         for(i=0;i<n;i++){
@@ -38,35 +36,30 @@ void main()
                         scan = i;
                         break;       
                         
+               }
                 }
-        
-        }
-        
-        printf("\nOrder of request served\n\n");
+          printf("\nOrder of request served\n\n");
         tot = 0;
         for(i=scan;i>=0;i--){
-                 //rai  tot = ioq[i+1] - ioq[i];
-                 tot = ioq[i] – ioq[i-1]; 
+                 
+                 tot = ioq[i] - ioq[i-1]; 
                  if(i==0) 
                         tot=ioq[i]-ioq[scan+1];
                 if(tot < 0)
                         tot = tot * -1;
-                //seek += tot;
+                
                 printf("%d\t%d\n",ioq[i],tot);
         }
                 
-      
-        for(i=scan+1;i<n;i++){
+             for(i=scan+1;i<n;i++){
                 tot = ioq[i+1] - ioq[i];
                 if(tot < 0)
                         tot = tot * -1;
-                //seek += tot;
+                
                 printf("%d\t%d\n",ioq[i],tot);
         }
         seek = ihead + ioq[n-1];
-        
         avgs = seek/(n-2);
-
         printf("\n\nTotal Seek time\t\t: %.2f",seek);
         printf("\nAverage seek time\t: %.2f\n\n",avgs);
         
