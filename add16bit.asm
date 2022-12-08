cf60a6decac30a2956e8dcd3d0dc11fe0924d760
DEMONLAWYERGT <91903192+DEMONLAWYERGT@users.noreply.github.com>
2022-12-08 10:38:55 +0530

    Create add16bit.asm

---
+++ add16bit.asm
@@ -0,0 +1,20 @@
+assume cs:code,ds:data
+data segment
+x dw 123h
+y dw 3456h
+r dw 0h
+rz db 0h
+data ends
+code segment
+start: mov ax,data
+mov ds,ax
+mov bx,y
+add ax,bx
+mov r,ax
+mov bl,0
+jnc l1
+mov bl,1
+l1: mov rz,bl
+int 03h
+code ends
+end start
